b3706edf7ed149ee075efac977211ed06921539a
abufahimkhan <abufahimkhan1@gmail.com>
2023-12-29 10:01:50 +0600

    eighth commit

--- src/Placecards.jsx
+++ src/Placecards.jsx
@@ -11,28 +11,28 @@ function Placecards() {
 
   return (
     <div className={`${menuOpen ? "menu-open" : ""}`}>
-
       <div className="cards-container">
-        {records.slice(0, showAllCards ? records.length : 3).map((getData) => (
-          <div className="cards" key={getData.id}>
+        {records.map((getData, index) => (
+          <div
+            className={`cards ${index >= 3 && !showAllCards ? "hidden-card" : ""}`}
+            key={getData.id}
+          >
             <p className="place-rating">⭐{getData.rating}</p>
             <img className="cards-img" src={getData.image} alt={getData.country} />
             <h3 className="title">{getData.country}</h3>
-            
             <p className="description">🔰 {getData.city}</p>
             <button className="seemore">See more</button>
           </div>
         ))}
       </div>
 
-      {records.length > 10 && (
+      {records.length > 3 && (
         <div className="see-more-container">
           <button className="see-more-btn" onClick={toggleCards}>
             {showAllCards ? "See Less" : "See More"}
           </button>
         </div>
       )}
-
     </div>
   );
 }
